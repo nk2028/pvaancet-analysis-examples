4751557f63bf12fb9857a798d1c8db5e0a21694d
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-05-29 23:03:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# pvaancet-analysis
+
+## Related articles
+
+- [利用程序分析廣韻反切](https://zhuanlan.zhihu.com/p/374098443)
---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget -nc https://raw.githubusercontent.com/nk2028/qieyun-data/9675d039f953393e248212354ea764eb3aad8dc3/%E9%9F%BB%E6%9B%B8/%E5%BB%A3%E9%9F%BB.csv
+wget -nc https://raw.githubusercontent.com/nk2028/qieyun-data/9b15b3486b4bd2b0e76162a980f260eed21f6905/%E5%8F%8D%E5%88%87%E9%9F%B3%E9%9F%BB%E5%9C%B0%E4%BD%8D/%E5%BB%A3%E9%9F%BB%E5%8F%8D%E5%88%87%E9%9F%B3%E9%9F%BB%E5%9C%B0%E4%BD%8D%E8%A1%A8.csv
